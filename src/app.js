06d1f9c5ee12c3c761bbb0084d24893af087ddcc
Alexander Shushunov <ashushunov@mail.ru>
2016-10-22 09:32:10 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const _ = require("lodash");
+
+console.log(_.VERSION);
+
+let {a, b} = {a: 2, b: 3};
+
+console.log(b);
---
+++ test/foo-spec.js
@@ -0,0 +1,15 @@
+"use strict";
+const foo = require("../src/foo");
+describe("Foo", function () {
+	beforeEach(function () {
+
+	});
+
+	afterEach(function () {
+
+	});
+
+	it("should ", function () {
+		expect(foo()).toBe(33);
+	});
+});
