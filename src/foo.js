7260955680752e60daaa3d914be007d8a464d8a0
Alexander Shushunov <ashushunov@mail.ru>
2016-10-22 16:18:44 +0300

    functional

--- src/foo.js
+++ src/foo.js
@@ -1,9 +1,21 @@
 "use strict";
 const _ = require("lodash");
 
-module.exports = class Foo {
+module.exports = {
 
-	foo() {
-		return 4;
+	birth(game, x, y) {
+		return _.concat(game, {x, y});
+	},
+
+	isAlive(game, x, y) {
+		return game.some(_.curry(_.isEqual)(_, {x, y}));
+	},
+
+	kill(game, x, y) {
+		return _.filter(game, _.negate(_.curry(_.isEqual)({x, y})));
+	},
+
+	step(game) {
+		return game;
 	}
 };
